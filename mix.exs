54d2c1da21b2a5283b4950d30818f1e1c20c8fb2
Chris Gast <cisco@mothership.local>
2019-02-20 14:03:14 -0500

    deployment stuff?

--- mix.exs
+++ mix.exs
@@ -21,6 +21,10 @@ defmodule Battleship.MixProject do
     ]
   end
 
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_),     do: ["lib"]
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
